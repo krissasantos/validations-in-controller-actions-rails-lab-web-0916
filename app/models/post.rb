a786eaef8b10e210353594f1579368d8c3f8d838
krissasantos <krissa.santos31@gmail.com>
2016-10-26 09:26:21 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%{category is not included!" }
-  validates :content, length: {minimum: 100}
+  validates :content, length: {minimum: 100} #yeayuh
 end
